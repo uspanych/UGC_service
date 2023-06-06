a738dbcce54c40639a307f3a756089255c4c0afd
Eldiyar <kndzh7@gmail.com>
2023-06-06 18:47:16 +0600

    test vertica

---
+++ vertica/commands.py
@@ -0,0 +1,15 @@
+SQL_CREATE_TABLE = """
+    CREATE TABLE views (
+    id IDENTITY,
+    user_id INTEGER NOT NULL,
+    movie_id VARCHAR(256) NOT NULL,
+    viewed_frame INTEGER NOT NULL
+    );
+"""
+
+SQL_CREATE_RECORD = """
+INSERT INTO views (user_id, movie_id, viewed_frame)
+{data};
+"""
+
+
