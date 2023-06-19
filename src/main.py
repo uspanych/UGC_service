6d4d95afad7688fb0bd3fa2bac8d18db0dd3af82
Sisilius <tolmachev-nikita@mail.ru>
2023-06-19 21:02:51 +0500

    Добавил мидлварь

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-import sentry_sdk
-import logstash
+from uuid import uuid4
 
+import logstash
+import sentry_sdk
 from aiokafka import AIOKafkaProducer
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.logger import logger
-from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.responses import ORJSONResponse
+from motor.motor_asyncio import AsyncIOMotorClient
 from redis.asyncio import Redis
 
-from api.v1 import views, likes, review, bookmarks
+from api.v1 import bookmarks, likes, review, views
 from core.config import settings
-from db import kafka, redis, mongo
-
+from db import kafka, mongo, redis
 
 logstash_handler = logstash.LogstashHandler('logstash', 5044, version=1)
 logger.addHandler(logstash_handler)
@@ -28,6 +28,15 @@ app = FastAPI(
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
 )
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await call_next(request)
+    request_id = response.headers.get("X-Request-Id")
+    if request_id is None:
+        response.headers["X-Request-Id"] = str(uuid4())
+    return response
 
 
 @app.on_event('startup')
@@ -66,4 +75,5 @@ async def test():
 app.include_router(views.router, prefix='/api/v1/views', tags=['views'])
 app.include_router(likes.router, prefix='/api/v1/likes', tags=['likes'])
 app.include_router(review.router, prefix='/api/v1/reviews', tags=['reviews'])
-app.include_router(bookmarks.router, prefix='/api/v1/bookmarks', tags=['bookmarks'])
+app.include_router(
+    bookmarks.router, prefix='/api/v1/bookmarks', tags=['bookmarks'])
